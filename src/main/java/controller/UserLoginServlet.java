6afb29557da1cd984a896534eea4f5b508616cea
zd3N08 <dounaritai@icloud.com>
2024-12-27 15:38:29 +0900

    ユーザー用ログインサーブレットの微修正
    UserDaoとUserのimport

--- src/main/java/controller/UserLoginServlet.java
+++ src/main/java/controller/UserLoginServlet.java
@@ -8,6 +8,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.UserDao;
+import domain.User;
+
 /**
  * Servlet implementation class UserLoginServlet
  */
